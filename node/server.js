b791209ae6df93a4e767dbc7cb3daecb54c1ab12
Tomek <belina.tomasz@gmail.com>
2015-12-31 15:31:27 +0100

    dodaj stylowanie

--- node/server.js
+++ node/server.js
@@ -5,7 +5,7 @@ http.createServer(function(request, response) {
 // tell the server what kind of file is coming at it
     response.writeHead(200, {"Content-Type": "text/plain"});
 // make the server output a message
-    response.write("Welcome to the future of JavaScript.");
+    response.write("Welcome to the future of JavaScript2 .");
 // End the server interaction
     response.end();
 }).listen(3000); // listening on port 3000
